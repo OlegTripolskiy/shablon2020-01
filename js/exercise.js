10e001a86e60f690074bd49df8b824dc00887710
OlegTripolskiy <bizprosto@gmail.com>
2020-06-09 23:14:51 +0300

    0.0.0.0.1  SORTABLE

--- js/exercise.js
+++ js/exercise.js
@@ -6,6 +6,21 @@ $("document").ready(function () {
       connectWith: ".sort", // Элементы ГРУПП можно перемещать между группами имеющими класс '.sort'
       items: "li:not(.title)" // Можно перемещать ТОЛЬКО элементы, не имеющие класс '.title'
     }).disableSelection();
-  
+    
+    // ==== ПРОВЕРКА УПРАЖНЕНИЯ ===============
+  /**/
+    $('#check-1').click(function(){
+        $('span.hidden').each(function(indx){
+            let el_hidd = $(this).html();
+            let el_ul = $(this).parents('.sort').attr('id');
+            if (el_hidd === el_ul) {
+                $(this).parent().css('backgroundColor','#9CEF00');
+                //alert(' СОВПАДАЕТ');
+            } else {
+                $(this).parent().css('backgroundColor','#FF9900');
+                //alert(' НЕТ');
+            }
+        });
+    });
 
 }); // Конец $("document").ready
